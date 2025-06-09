1d35411c04d3e7b17a5ce159674a047c68007b7b
semrebitaye <asimrobitaye23@gmail.com>
2025-06-09 22:25:08 +0300

    get  user functionality restored from get_all_user branch

--- controllers/userController.go
+++ controllers/userController.go
@@ -6,42 +6,28 @@ import (
 	"log"
 	"net/http"
 	"user_crud_api/models"
-
-	"github.com/gorilla/mux"
-	"golang.org/x/crypto/bcrypt"
 )
 
-func CreateUser(db *sql.DB) http.HandlerFunc {
+func GetUsers(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var u models.User
-		err := json.NewDecoder(r.Body).Decode(&u)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-		}
-		hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-		}
-
-		err = db.QueryRow("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id", u.FirstName, u.LastName, u.Email, string(hash)).Scan(&u.ID)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-		}
-
-		json.NewEncoder(w).Encode(u)
-	}
-}
-
-func DeleteUser(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id := vars["id"]
-
-		_, err := db.Exec("DELETE FROM users WHERE id = $1", id)
+		rows, err := db.Query("SELECT * FROM users")
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer rows.Close()
 
-		json.NewEncoder(w).Encode("User Deleted")
+		users := []models.User{}
+		for rows.Next() {
+			var u models.User
+			if err := rows.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Email, &u.Password); err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(users)
 	}
 }
