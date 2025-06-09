2176df6ebcbe5b11a68cee69061b29ddd8562c0b
semrebitaye <asimrobitaye23@gmail.com>
2025-06-09 18:14:19 +0300

    delete user function added

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,24 @@
 package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func DeleteUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		_, err := db.Exec("DELETE FROM users WHERE id = $1", id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode("User Deleted")
+	}
+}
