11e0e772aac88e3b894439dd43bf19c441fabc03
semrebitaye <asimrobitaye23@gmail.com>
2025-06-09 17:21:50 +0300

    modified

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	initializer "user_crud_api/initializers"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	initializer.LoadEnvVariable()
 	initializer.Connect()
 
+	r := mux.NewRouter()
+
 	// user := &models.User{ID: 1, FirstName: "man", LastName: "manega", Email: "man@man", Password: "manegaga"}
 	// fmt.Println(user)
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
