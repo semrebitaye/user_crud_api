5920fdb205c91eb307a4f095229632857b8815c9
semrebitaye <asimrobitaye23@gmail.com>
2025-06-09 17:47:45 +0300

    create user modified

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"database/sql"
 	"encoding/json"
+
 	"log"
 	"net/http"
 	"user_crud_api/models"
@@ -45,5 +46,19 @@ func DeleteUser(db *sql.DB) http.HandlerFunc {
 		}
 
 		json.NewEncoder(w).Encode("User Deleted")
+	}
+}
+
+func CreateUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var u models.User
+		json.NewDecoder(r.Body).Decode(&u)
+
+		err := db.QueryRow("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2) RETURNING id", u.FirstName, u.LastName, u.Email, u.Password).Scan(&u.ID)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		json.NewEncoder(w).Encode(u)
 	}
 }
--- main.go
+++ main.go
@@ -18,10 +18,14 @@ func main() {
 
 	r := mux.NewRouter()
 <<<<<<< HEAD
+<<<<<<< HEAD
 	r.HandleFunc("/get", controllers.GetUsers(db)).Methods("GET")
 =======
 	r.HandleFunc("/delete/{id}", controllers.DeleteUser(db)).Methods("DELETE")
 >>>>>>> 2176df6 (delete user function added)
+=======
+	r.HandleFunc(":/8080", controllers.CreateUser(db))
+>>>>>>> 7a7112f (Create User function added)
 
 	// user := &models.User{ID: 1, FirstName: "man", LastName: "manega", Email: "man@man", Password: "manegaga"}
 	// fmt.Println(user)
