7a7112fa2dd611ad1390b70e5761d484761c670d
semrebitaye <asimrobitaye23@gmail.com>
2025-06-09 17:47:45 +0300

    Create User function added

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"user_crud_api/models"
+)
+
+func CreateUser(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var u models.User
+		json.NewDecoder(r.Body).Decode(&u)
+
+		err := db.QueryRow("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2) RETURNING id", u.FirstName, u.LastName, u.Email, u.Password).Scan(&u.ID)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		json.NewEncoder(w).Encode(u)
+	}
+}
